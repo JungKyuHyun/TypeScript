edcfaabd31ebe7c8f9281aeee5659ec9cef25b54
정규현 (Jacob) <kh.jung@ecubelabs.com>
2019-12-20 20:02:49 +0900

    Init index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
+// 1. npm i
+// 2. npm start
+// 3. Write your code!
 Object.defineProperty(exports, "__esModule", { value: true });
+console.log("test-code");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// 1. npm i
+// 2. npm start
+// 3. Write your code!
 
+console.log("test-code");
 
 export {};
